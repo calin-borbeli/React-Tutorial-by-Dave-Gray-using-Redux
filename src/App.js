a3018d876e6d201eedd4f3acdec9b98ec564f3b6
calin-borbeli <calin@calinborbeli.com.au>
2022-04-09 10:16:27 +1000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import Layout from "./Layout";
+import Home from "./Home";
+import NewPost from "./NewPost";
+import EditPost from "./EditPost";
+import PostPage from "./PostPage";
+import About from "./About";
+import Missing from "./Missing";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path="/" element={<Layout />}>
+				<Route index element={<Home />} />
+				<Route path="post" element={<NewPost />} />
+				<Route path="post/:id" element={<PostPage />} />
+				<Route path="edit/:id" index element={<EditPost />} />
+				<Route path="about" element={<About />} />
+				<Route path="*" element={<Missing />} />
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
